8e5ee2107242f13c7a2bf42e3c441c59d8846b49
Edilson Lima <edilson.lima@nextem.com.br>
2020-01-31 10:24:46 -0400

    Adicionando .configure() nas configurações do reactotron

--- src/config/ReactotronConfig.js
+++ src/config/ReactotronConfig.js
@@ -2,7 +2,7 @@ import Reactotron from 'reactotron-react-js';
 import { reactotronRedux } from 'reactotron-redux'
 
 if (process.env.NODE_ENV === 'development') {
-  const tron = Reactotron
+  const tron = Reactotron.configure()
     .use(reactotronRedux())
     .connect();
 

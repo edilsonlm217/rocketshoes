509ad627154e93447397c49aa1b4b6ef361bb230
Edilson Lima <edilson.lima@nextem.com.br>
2020-01-31 15:11:14 -0400

    consumindo a store na view de cart

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import {MdRemoveCircleOutline, MdAddCircleOutline, MdDelete} from 'react-icons/md'
 
 import { Container, ProductTable, Total } from './styles';
 
 export default function Cart() {
+  const cart = useSelector(state => state.cart);
+  const dispatch = useDispatch();
+
   return (
     <Container>
       <ProductTable>
@@ -17,37 +21,39 @@ export default function Cart() {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>
-              <img
-                src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-5-feminino/26/HZM-1729-026/HZM-1729-026_detalhe2.jpg?ims=326x"
-                alt="Tênis"
-              />
-            </td>
-            <td>
-              <strong>Tênis muito massa</strong>
-              <span>R$129,90</span>
-            </td>
-            <td>
-              <div>
-                <button type="button">
-                  <MdRemoveCircleOutline size={20} color="#7159c1"/>
+          { cart.map(product => (
+            <tr>
+              <td>
+                <img
+                  src={product.image}
+                  alt={product.title}
+                />
+              </td>
+              <td>
+                <strong>{product.title}</strong>
+                <span>{product.formattedPrice}</span>
+              </td>
+              <td>
+                <div>
+                  <button type="button">
+                    <MdRemoveCircleOutline size={20} color="#7159c1"/>
+                  </button>
+                  <input type="number" readOnly value={product.amout}/>
+                  <button type="button">
+                    <MdAddCircleOutline size={20} color="#7159c1"/>
+                  </button>
+                </div>
+              </td>
+              <td>
+                <strong>R$258,80</strong>
+              </td>
+              <td>
+                <button type="button ">
+                  <MdDelete size={20} onClick={() => dispatch({ type: 'REMOVE_FROM_CART', id: product.id})} color="7159c1"/>
                 </button>
-                <input type="number" readOnly value={2}/>
-                <button type="button">
-                  <MdAddCircleOutline size={20} color="#7159c1"/>
-                </button>
-              </div>
-            </td>
-            <td>
-              <strong>R$258,80</strong>
-            </td>
-            <td>
-              <button type="button ">
-                <MdDelete size={20} color="7159c1"/>
-              </button>
-            </td>
-          </tr>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </ProductTable>
 

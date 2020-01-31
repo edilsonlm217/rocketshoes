208251cf5a51bfe65c02378cef74987afc0d68d9
Edilson Lima <edilson.lima@nextem.com.br>
2020-01-31 15:10:24 -0400

    criação da action REMOVE_FROM_CART

--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -1,7 +1,30 @@
+import produce from 'immer';
+
 export default function cart(state = [], action) {
   switch (action.type) {
     case 'ADD_TO_CART':
-      return [ ...state, action.product ];
+      return produce(state, draft => {
+        const productIndex = draft.findIndex(p => p.id === action.product.id);
+        
+        if (productIndex >= 0) {
+          draft[productIndex].amout += 1;
+        } else {
+          draft.push({
+            ...action.product, 
+            amout: 1
+          });
+        }
+      });
+
+    case 'REMOVE_FROM_CART':
+      return produce(state, draft => {
+        const productIndex = draft.findIndex(p => p.id === action.id);
+
+        if (productIndex >= 0) {
+          draft.splice(productIndex, 1);
+        }
+      });
+
     default:
       return state;
   }

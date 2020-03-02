f12407290f0c2215049da2a1f93532bd4ec6437e
Edilson Lima <edilson.lima@nextem.com.br>
2020-03-02 08:07:19 -0400

    Implementação calculo subtotal por item do carrinho

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,13 +1,33 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import {MdRemoveCircleOutline, MdAddCircleOutline, MdDelete} from 'react-icons/md'
+import { formatPrice } from '../../util/format';
 
 import * as CartActions from '../../store/modules/cart/actions';
 
 import { Container, ProductTable, Total } from './styles';
 
 export default function Cart() {
-  const cart = useSelector(state => state.cart);
+  const cart = useSelector(state => {
+    if (state.cart.length === 0) {
+      return ({
+        cart: state.cart,
+        total: formatPrice(0),
+      });
+      
+    } else {
+      return ({
+        cart: state.cart.map(product => ({
+          ...product,
+          sub_total: formatPrice(product.price * product.amount),
+        })),
+        total: 1234,
+      });
+    }
+  });
+
+  console.tron.log(cart);
+
   const dispatch = useDispatch();
 
   function increment(product) {
@@ -31,7 +51,7 @@ export default function Cart() {
           </tr>
         </thead>
         <tbody>
-          { cart.map(product => (
+          { cart.cart.map(product => (
             <tr>
               <td>
                 <img
@@ -55,7 +75,7 @@ export default function Cart() {
                 </div>
               </td>
               <td>
-                <strong>R$258,80</strong>
+                <strong>{product.sub_total}</strong>
               </td>
               <td>
                 <button type="button ">
@@ -71,7 +91,7 @@ export default function Cart() {
         <button type="button">Finalizar pedido</button>
         <Total>
           <span>TOTAL</span>
-          <strong>R$1920,28</strong>
+          <strong>{cart.total}</strong>
         </Total>
       </footer>
     </Container>
